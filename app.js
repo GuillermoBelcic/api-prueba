c76a1ef8d858f3677f2f0b34d4e3e6ccf4f4b16d
GuilleB <guillermo.belcic@gmail.com>
2021-01-12 16:46:43 +0100

    Heroku port setting

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = 9999;
+const port = process.env.PORT || 9999;
 app.get("/", (req, res) => {
     res.send("Hello, World!");
 });
